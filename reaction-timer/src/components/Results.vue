785316acf165c7aadff85c97a899ccd2bb328d7e
Ibnul Hasan <ibnul.ku.cse@gmail.com>
2021-08-06 23:30:47 +0600

    first commit to upload

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <p>Reaction Time: {{score}}</p>
+    <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250 ){
+            this.rank = "Ninja Fingers"
+        }
+        else if(this.score < 400){
+            this.rank = "Rapid reflaxes"
+        }
+        else{
+            this.rank = "Snail pace..."
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0fea0f;
+        font-weight: bold;
+    }
+</style>
